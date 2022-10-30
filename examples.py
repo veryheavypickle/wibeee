caf86a25e1d0331fb80c5ac78f12ee09e7c45dac
veryheavypickle <xavier.carroll01@gmail.com>
2022-10-30 05:41:07 +0100

    show verbose

--- examples.py
+++ examples.py
@@ -1,7 +1,7 @@
 import src
 
 if __name__ == '__main__':
-    WiBeee = src.WiBeee('192.168.1.145')
+    WiBeee = src.WiBeee('192.168.1.145', verbose=True)
     while True:
         print(WiBeee.power())
 
