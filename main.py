2da52c93cea88f4a8bbef1ec40c597c9f1e67f20
veryheavypickle <xavier.carroll01@gmail.com>
2022-10-30 02:39:47 +0200

    Added data points to get

--- main.py
+++ main.py
@@ -2,10 +2,5 @@ import src
 
 if __name__ == '__main__':
     WiBeee = src.WiBeee("192.168.1.145")
-    status = WiBeee.getStatus()
-    for stat in status:
-        print("{0}: {1}".format(stat, status[stat]))
-
-    print("")
-    print(status["fase1_p_activa"])
+    print(WiBeee.power())
 
--- src/wibeee/__init__.py
+++ src/wibeee/__init__.py
@@ -6,7 +6,7 @@ import time
 
 
 class WiBeee:
-    def __init__(self, host=None, port=80, timeout=10.0, ucm='async_httpx'):
+    def __init__(self, host=None, port=80, timeout=10.0):
         self.host = host
         self.port = port
         self.timeout = timeout
@@ -25,11 +25,12 @@ class WiBeee:
                     request, timeout=self.timeout
                 )
             return response.text
-        except requests.exceptions.Timeout:
-            raise errors.BadHostName("The WiBeee device seems to be down, try autodiscovery to get the correct url")
         except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
             time.sleep(1)
             return self.callURL(url, attempts + 1)
+        except requests.exceptions.Timeout as e:
+            print(e.response)
+            raise errors.BadHostName("The WiBeee device seems to be down, try autodiscovery to get the correct url")
 
     def autoDiscover(self):
         hosts = utils.getActiveHosts()
@@ -40,6 +41,26 @@ class WiBeee:
                 return host
         raise errors.NoWiBeeeDevices("No WiBee Devices were found on the local network")
 
+    def getHost(self):
+        return self.host
+
     def getStatus(self):
         url = self.baseURL + "/en/status.xml"
         return xmltodict.parse(self.callURL(url))["response"]
+
+    def voltage(self, phase=1):
+        # rms is root-mean-square
+        key = "fase{}_vrms".format(phase)
+        return float(self.getStatus()[key])
+
+    def current(self, phase=1):
+        key = "fase{}_irms".format(phase)
+        return float(self.getStatus()[key])
+
+    def frequency(self, phase=1):
+        key = "fase{}_frecuencia".format(phase)
+        return float(self.getStatus()[key])
+
+    def power(self, phase=1):
+        key = "fase{}_p_activa".format(phase)
+        return float(self.getStatus()[key])
